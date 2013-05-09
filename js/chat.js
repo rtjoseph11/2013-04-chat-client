59c98dc85ca2058596ea1634d7e61f72fd47b017
Hack Reactor Students <github@hackreactor.com>
2013-05-08 17:01:15 -0700

    got the chat client to send and receive messages

--- js/chat.js
+++ js/chat.js
@@ -1,25 +1,42 @@
 $(document).ready(function(){
-  $('#testGet').on('click', function(){
-    console.log('running get');
-    var $body = $('body');
+  var sendMessage = function(input, username){
+    $.ajax('https://api.parse.com/1/classes/tuckertest', {
+      contentType: 'application/json',
+      data: '{"username": "' + username + '", "text": "' + input + '"}',
+      type: 'POST',
+      success: function(data){
+        $('#messageText').val('');
+        console.log('message sent!');
+      },
+      error: function(data){
+        console.log('an error occured!', data);
+      }
+    });
+  };
+  var getMessages = function(){
     $.ajax('https://api.parse.com/1/classes/tuckertest', {
       contentType: 'application/json',
       success: function(data){
-        $body.append(data.results[0].message);
+        for (var i = messages.length; i < data.results.length; i++){
+          var message = data.results[i].username + ' (' +jQuery.timeago(data.results[i].createdAt) + '): ' + data.results[i].text;
+          var $li = $('<li></li>').text(message);
+         $('#main ul').append($li);
+        }
+        messages = data.results;
       },
       error: function(data) {
         console.log('message request failed');
       }
     });
+  };
+  var messages = [];
+  var chatInterval = setInterval(getMessages, 1000);
+  $('#sendMessage').on('click', function(){
+    sendMessage($('#messageText').val(), window.location.search.slice(10));
   });
-  $('#testPost').on('click', function(){
-    $.ajax('https://api.parse.com/1/classes/tuckertest', {
-      contentType: 'application/json',
-      data: '{"username": "tucker", "message": "hello World"}',
-      type: 'POST',
-      success: function(data){
-        console.log('posted message succesfully');
-      }
-    });
+  $('#messageText').on('keyup', function(event){
+    if (event.which === 13){
+      sendMessage($('#messageText').val(), window.location.search.slice(10));
+    }
   });
 });
